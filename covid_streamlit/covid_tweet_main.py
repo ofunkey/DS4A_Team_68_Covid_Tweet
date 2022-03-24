351870a79497293a4674635cde6d7f26e605acf5
Olufunke Olaleye <ofunkey@hotmail.com>
2022-03-24 17:53:18 -0400

    Add files via upload

--- covid_streamlit/covid_tweet_main.py
+++ covid_streamlit/covid_tweet_main.py
@@ -3,11 +3,11 @@
 #https://www.hackerearth.com/practice/
 #https://amueller.github.io/word_cloud/auto_examples/colored_by_group.html
 
-import yoseph
-import funke
-import arianna
-import suzan_v2
-import jasiya
+import prediction
+import mywordcloud
+import heatmap
+import dataframe
+import sentiment
 import home_page
 import streamlit as st
 
@@ -21,7 +21,7 @@ hide_menu = """
 
     
 footer:before{
-    content: 'Created by Team 68: Yoseph Dawit, Arianna Cooper, Funke Olaleye, Suzan Pham, and Jasiya Thompson';
+    content: 'Created by Team 68: Arianna Cooper, Yoseph Dawit, Funke Olaleye and Suzan Pham';
     display: block;
     position:relative;
     color: white;
@@ -31,7 +31,7 @@ footer:before{
 """
 
 
-pages = {'About':home_page, 'DataFrame':suzan_v2, 'Sentiment':jasiya, 'WordCloud':funke, 'Heatmap':arianna, 'Prediction':yoseph}
+pages = {'About':home_page, 'DataFrame':dataframe, 'Sentiment':sentiment, 'WordCloud':mywordcloud, 'Heatmap':heatmap, 'Prediction':prediction}
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
