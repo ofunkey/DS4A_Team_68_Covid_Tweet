a1338daf5dba70bea3049855f471fa0709f1a3f8
Olufunke Olaleye <ofunkey@hotmail.com>
2022-03-25 00:26:59 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
 mkdir -p ~/.streamlit/
+
+[theme]
+primaryColor="#d33682"
+backgroundColor="#002b36"
+secondaryBackgroundColor="#586e75"
+textColor="#fafafa"
+font="serif"
+
 echo "\
 [server]\n\
 headless = true\n\
