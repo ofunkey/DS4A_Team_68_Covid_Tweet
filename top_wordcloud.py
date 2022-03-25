ed71220c2798f5c681b96725b5ff551798c6b35f
Olufunke Olaleye <ofunkey@hotmail.com>
2022-03-24 22:33:48 -0400

    added nltk_download

--- top_wordcloud.py
+++ top_wordcloud.py
@@ -35,6 +35,7 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 from textblob import TextBlob
 from textblob import Word
+nltk.download()
 nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('punkt')
