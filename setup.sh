2da3f3eaac79a70b8745f852222a9c53edc903c2
Olufunke Olaleye <ofunkey@hotmail.com>
2022-03-30 13:24:32 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,13 +7,3 @@ enableCORS = false\n\
 \n\
 " 
 > ~/.streamlit/config.toml
-
-echo "\
-[theme]\n\
-primaryColor = \"#d33682\"\n\
-backgroundColor = \"#002b36\"\n\
-secondaryBackgroundColor = \"#586e75\"\n\
-textColor = \"#fafafa\"\n\
-font = \"serif\"\n\
-\n\
-" > ~/.streamlit/config.toml
