1c4f9827008cbc9f4b0a5534f6ca97ce05ed8ffa
Olufunke Olaleye <ofunkey@hotmail.com>
2022-03-30 12:58:54 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,21 @@
 mkdir -p ~/.streamlit/
-
 echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
+";
+
+sleep 2;
+
+echo "\
+[theme]\n\
+primaryColor = \"#d33682\"\n\
+backgroundColor = \"#002b36\"\n\
+secondaryBackgroundColor = \"#586e75\"\n\
+textColor = \"#fafafa\"\n\
+font = \"serif\"\n\
+\n\
+
 " > ~/.streamlit/config.toml
